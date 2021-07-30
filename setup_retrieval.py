54bb33624deb24fe5535cf676c5dc871d3a54469
David Tomás <dtomas@dlsi.ua.es>
2021-07-30 13:42:33 +0200

    Create setup_retrieval.py

---
+++ setup_retrieval.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name = 'table_retrieval',
+    version = '0.1',
+    description = 'Given the similarity between CSV tables, retrieve the top "n" tables for a given table.',
+    author = 'David Tomás',
+    author_email = 'dtomas@dlsi.ua.es',
+    packages = ['table_retrieval'])
