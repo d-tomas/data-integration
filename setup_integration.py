cad189b95a78df9f574cb47b8dce4e6eb9fba209
David Tomás <dtomas@dlsi.ua.es>
2021-08-02 01:04:33 +0200

    Add files via upload

---
+++ setup_integration.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name = 'table_integration',
+    version = '0.1',
+    description = 'Given the similarity between CSV tables, identify which tables can be integrated by means of union and join operations.',
+    author = 'David Tomás',
+    author_email = 'dtomas@dlsi.ua.es',
+    packages = ['table_integration'])
