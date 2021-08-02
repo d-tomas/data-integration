a572ed6c5c6b4d399fad44306027638d07cc2bf9
David Tomás <dtomas@dlsi.ua.es>
2021-08-02 10:33:47 +0200

    Update setup_similarity.py

--- setup_similarity.py
+++ setup_similarity.py
@@ -6,4 +6,4 @@ setup(name = 'table_similarity',
     author = 'David Tomás',
     author_email = 'dtomas@dlsi.ua.es',
     packages = ['table_similarity'],
-    install_requires = ['numpy>=1.15.1', 'python-Levenshtein>=0.12.0', 'scipy>=1.1.0', 'gensim>=3.8.0', 'transformers>=4.9.1'])
+    install_requires = ['numpy>=1.15.1', 'python-Levenshtein>=0.12.0', 'scipy>=1.1.0', 'gensim>=3.8.0', 'transformers>=4.9.1', 'torch>=1.7.1'])
